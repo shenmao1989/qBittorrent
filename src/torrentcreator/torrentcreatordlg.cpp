97f8de3dc7e5a4f79b8a1f24a19ac6408c644736
shenmao1989 <shenmao1989@gmail.com>
2012-08-30 00:19:26 +0800

    9 每次都得重新找到种子文件 重新载入

--- src/torrentcreator/torrentcreatordlg.cpp
+++ src/torrentcreator/torrentcreatordlg.cpp
@@ -160,7 +160,7 @@ void TorrentCreatorDlg::handleCreationSuccess(QString path, QString branch_path)
     // Create save path temp data
     boost::intrusive_ptr<torrent_info> t;
     try {
-      t = new torrent_info(path.toStdString().data());
+      t = new torrent_info(path.toUtf8().constData());
     } catch(std::exception&) {
       QMessageBox::critical(0, tr("Torrent creation"), tr("Created torrent file is invalid. It won't be added to download list."));
       return;
